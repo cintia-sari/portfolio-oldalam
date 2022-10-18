e31adeb360164425c09b63ee004929430c4340ee
Sári Cintia <sari.cintia1996@gmail.com>
2022-10-18 11:00:25 +0200

    kisebb javítások

---
+++ css/index.js
@@ -0,0 +1,9 @@
+const menuLinks = document.querySelectorAll('.menu-box a');
+
+function closeHamburgerMenu() {
+    document.querySelector('#menu__toggle').checked = false;
+}
+
+for (let link of menuLinks) {
+    link.addEventListener('click', closeHamburgerMenu);
+}
